fd295ede8fe23aaf1700a3b52c97c74d2cd6f88b
Kaique Ferreira <kaiquerbn@gmail.com>
2024-06-16 15:29:53 -0300

    feat: chamada do backend criada

--- src/app/component/default-login-layou/default-login-layou.component.ts
+++ src/app/component/default-login-layou/default-login-layou.component.ts
@@ -1,14 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layou',
   standalone: true,
   imports: [],
   templateUrl: './default-login-layou.component.html',
-  styleUrl: './default-login-layou.component.scss'
+  styleUrl: './default-login-layou.component.scss',
 })
 export class DefaultLoginLayouComponent {
-  @Input() title: string = "";
-  @Input() primaryBtnText: string = "";
-  @Input() secondaryBtnText: string = "";
+  @Input() title: string = '';
+  @Input() primaryBtnText: string = '';
+  @Input() secondaryBtnText: string = '';
+  @Input() disablePrimaryBtn: boolean = true;
+
+  //submit para enviar dados de login.
+  @Output('submit') onSubmit = new EventEmitter();
+  submit() {
+    this.onSubmit.emit();
+  }
+  //navegar para a página de cadastro
+  @Output('navigate') onNavigate = new EventEmitter();
+  navigate() {
+    this.onNavigate.emit();
+  }
 }
